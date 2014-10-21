3665328cc05b9b881dfd4bcfbe7ebd8cd2690513
Blaz Solar <solarblaz@gmail.com>
2014-10-21 18:37:39 +0200

    Adding debug info to debug drawer

--- app/src/debug/java/com/github/blazsolar/chuck/ui/DebugAppContainer.java
+++ app/src/debug/java/com/github/blazsolar/chuck/ui/DebugAppContainer.java
@@ -1,8 +1,12 @@
 package com.github.blazsolar.chuck.ui;
 
 import android.app.Activity;
+import android.os.Build;
 import android.support.v4.widget.DrawerLayout;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.blazsolar.chuck.R;
 
@@ -21,6 +25,13 @@ public class DebugAppContainer implements AppContainer {
     @InjectView(R.id.debug_drawer_layout) DrawerLayout drawerLayout;
     @InjectView(R.id.debug_content) ViewGroup content;
 
+    @InjectView(R.id.debug_device_make) TextView deviceMakeView;
+    @InjectView(R.id.debug_device_model) TextView deviceModelView;
+    @InjectView(R.id.debug_device_resolution) TextView deviceResolutionView;
+    @InjectView(R.id.debug_device_density) TextView deviceDensityView;
+    @InjectView(R.id.debug_device_release) TextView deviceReleaseView;
+    @InjectView(R.id.debug_device_api) TextView deviceApiView;
+
     @Inject public DebugAppContainer() {
     }
 
@@ -30,8 +41,47 @@ public class DebugAppContainer implements AppContainer {
 
         activity.setContentView(R.layout.debug_activity_frame);
 
+        // Manually find the debug drawer and inflate the drawer layout inside of it.
+        ViewGroup drawer = ButterKnife.findById(activity, R.id.debug_drawer);
+        LayoutInflater.from(activity).inflate(R.layout.debug_drawer_content, drawer);
+
         ButterKnife.inject(this, activity);
+
+        setupDeviceSection();
 
         return content;
     }
+
+    private void setupDeviceSection() {
+        DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
+        String densityBucket = getDensityString(displayMetrics);
+        deviceMakeView.setText(Build.MANUFACTURER);
+        deviceModelView.setText(Build.MODEL);
+        deviceResolutionView.setText(displayMetrics.heightPixels + "x" + displayMetrics.widthPixels);
+        deviceDensityView.setText(displayMetrics.densityDpi + "dpi (" + densityBucket + ")");
+        deviceReleaseView.setText(Build.VERSION.RELEASE);
+        deviceApiView.setText(String.valueOf(Build.VERSION.SDK_INT));
+    }
+
+    private static String getDensityString(DisplayMetrics displayMetrics) {
+        switch (displayMetrics.densityDpi) {
+            case DisplayMetrics.DENSITY_LOW:
+                return "ldpi";
+            case DisplayMetrics.DENSITY_MEDIUM:
+                return "mdpi";
+            case DisplayMetrics.DENSITY_HIGH:
+                return "hdpi";
+            case DisplayMetrics.DENSITY_XHIGH:
+                return "xhdpi";
+            case DisplayMetrics.DENSITY_XXHIGH:
+                return "xxhdpi";
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                return "xxxhdpi";
+            case DisplayMetrics.DENSITY_TV:
+                return "tvdpi";
+            default:
+                return "unknown";
+        }
+    }
+
 }

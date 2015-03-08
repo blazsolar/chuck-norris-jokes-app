369c9b69a70ffcdd012abd7b2028e593ad2a8508
Blaz Solar <solarblaz@gmail.com>
2015-03-08 17:22:34 +0100

    Test run fix

--- config/ci/circle/run_tests.sh
+++ config/ci/circle/run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ "CIRCLE_NODE_INDEX" = "0" ]
+if [ $CIRCLE_NODE_INDEX -eq 0 ]
 then
+    echo "Check"
     ./gradlew check
-elif [ "CIRCLE_NODE_INDEX" = "1" ]
+elif [ $CIRCLE_NODE_INDEX = 1 ]
 then
+    echo "Connected check"
     ./gradlew connectedCheck
 fi
